91b54810559f17b86598f2aba2a9eec85e31df83
vonvick <victor.nwaiwu9@gmail.com>
2016-09-20 20:31:12 +0100

    create the forms.py file and edit the controllers.py file to include the form class.

--- app/controllers.py
+++ app/controllers.py
@@ -1,7 +1,9 @@
 # app/controllers.py
 
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app import app
+from .forms import EmailPasswordForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -31,6 +33,13 @@ def books():
     ]
     return render_template('books.html', books = books)
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template('login.html')
+    form = EmailPasswordForm()
+    if form.validate_on_submit():
+
+        # Check the password and log the user in
+        
+
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
