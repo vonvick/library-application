74244a42966dc661d4e2f2786977760eeb42f926
vonvick <victor.nwaiwu9@gmail.com>
2016-09-20 15:19:14 +0100

    create the initialization file for the admin the admin blueprint has been created with the __init__.py file

---
+++ app/admin/__init__.py
@@ -0,0 +1,13 @@
+# app/admin/__init__.py
+
+from flask import Blueprint
+
+admin = Blueprint(
+
+  'admin',
+  __name__,
+  template_folder = 'templates',
+  static_folder   = 'static'
+)
+
+from . import views
