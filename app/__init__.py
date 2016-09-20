b1c3288169d90bcdb8a49e48fdf9ac6a3b15ee52
vonvick <victor.nwaiwu9@gmail.com>
2016-09-20 20:00:55 +0100

    delete files and resolve merge conflict error

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,7 @@
 # app/__init__.py
-<<<<<<< HEAD
+
 from flask import Flask
 
 app = Flask(__name__)
 
 from app import controllers
-=======
-
-from flask import Flask
-from .public import public
-from .admin import admin
-
-
-app = Flask(__name__)
-app.config.from_object('app.admin.config')
-
-# Puts the API blueprint.
-app.register_blueprint(public, url_prefix = '/public')
-app.register_blueprint(admin, url_prefix = '/admin')
->>>>>>> 9bceb7515570fce8e93504ca4b8012168234248b
--- run.py
+++ run.py
@@ -1,8 +1,4 @@
 #!flask/bin/python
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 9bceb7515570fce8e93504ca4b8012168234248b
 from app import app
 app.run(debug=True)
