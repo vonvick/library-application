20723ccde0de633ae6b97225dcb0f1c21b166a4c
vonvick <victor.nwaiwu9@gmail.com>
2016-09-20 15:24:03 +0100

    edit the app initialization file to add the admin edit the __init__.py to include the blueprint for the admin end of the application

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@
 
 from flask import Flask
 from .public import public
+from .admin import admins
 
 
 app = Flask(__name__)
 
-# Puts the API blueprint on api.U2FtIEJsYWNr.com.
+# Puts the API blueprint.
 app.register_blueprint(public, url_prefix = '/public')
+app.register_blueprint(public, url_prefix = '/admin')
