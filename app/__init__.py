da2f11743fb3358193a0dcdead16cc685549ef72
vonvick <victor.nwaiwu9@gmail.com>
2016-09-21 17:36:01 +0100

    create the db_create.py file create the file that creates the database and also instantiates the table classes in model.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 # app/__init__.py
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
-from app import controllers
+
+from app import controllers, models
