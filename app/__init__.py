ccc4e48defde8a3b3ee75bf8ba67b422e74d16cd
vonvick <victor.nwaiwu9@gmail.com>
2016-09-21 19:54:20 +0100

    delete the bcrypt module from the __init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,11 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
 
 
 from app import controllers, models
