0c02726f276e0ae371870d9a57f9ec81eabb87a9
vonvick <victor.nwaiwu9@gmail.com>
2016-09-21 12:04:32 +0100

    add the register route to the controllers.py file Add the register route and also edit the login route

--- app/controllers.py
+++ app/controllers.py
@@ -1,8 +1,9 @@
 # app/controllers.py
 
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
+from flask_login import login_user, logout_user, current_user, login_required
 from app import app
-from .forms import EmailPasswordForm
+from .forms import EmailPasswordForm, RegistrationForm
 
 
 @app.route('/')
@@ -33,15 +34,25 @@ def books():
     ]
     return render_template('books.html', books = books)
 
-@app.route('/login', methods=["GET", "POST"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = EmailPasswordForm()
-    if form.validate_on_submit():
-
+    if request.method == 'POST' and form.validate():
         # Check the password and log the user in
-        if form.email == 'vonvikky@gmail.com' and form.password == 'olusegun':
-            return true
-        
+        if form.email.data == 'vonvikky@gmail.com' and form.password.data == 'olusegun':
+            return redirect(url_for('index'))
+        return render_template('login.html', form = form)
+    return render_template('login.html', form = form)
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    registerform = RegistrationForm()
+    if request.method == 'POST' and registerform.validate():
+        user = User(registerform.firstname.data, registerform.lastname.data, registerform.email.data,
+                    form.password.data)
+        # checks the data submitted with format
+        flash('Thank you for registering', user.firstname)
+        # check if a user with such email already exists, If not
+        # insert form data into the database and create a new user
         return redirect(url_for('index'))
-    return render_template('login.html', form=form)
+    return render_template('signup.html', form = registerform)
