2c8ebd79ccd20a9e662c402bda81e6cdc34c0972
vonvick <victor.nwaiwu9@gmail.com>
2016-09-22 12:03:26 +0100

    edit the config.py file

--- config.py
+++ config.py
@@ -1,33 +1,33 @@
 # config.py
 
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+# import os
+# basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-class Config(object):
-    DEBUG = False
-    TESTING = False
-    WTF_CSRF_ENABLED = True
-    SECRET_KEY = 'AdonbilivitOlusegun'
+# class Config(object):
+#     DEBUG = False
+#     TESTING = False
+#     WTF_CSRF_ENABLED = True
+#     SECRET_KEY = 'AdonbilivitOlusegun'
 
 
-class ProductionConfig(Config):
-    DEBUG = False
+# class ProductionConfig(Config):
+#     DEBUG = False
 
 
-class StagingConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
+# class StagingConfig(Config):
+#     DEVELOPMENT = True
+#     DEBUG = True
 
 
-class DevelopmentConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
+# class DevelopmentConfig(Config):
+#     DEVELOPMENT = True
+#     DEBUG = True
 
 
-class TestingConfig(Config):
-    TESTING = True
+# class TestingConfig(Config):
+#     TESTING = True
 
 
-# WTF_CSRF_ENABLED = True
-# SECRET_KEY = 'AdonbilivitOlusegun'
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'AdonbilivitOlusegun'
