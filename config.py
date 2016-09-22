70bca5ae933222b8a4ce6a27e38a6f693b819631
vonvick <victor.nwaiwu9@gmail.com>
2016-09-22 11:03:32 +0100

    edit the config.py file edit the config.py file to detect the different environment

--- config.py
+++ config.py
@@ -1,4 +1,33 @@
 # config.py
 
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'AdonbilivitOlusegun'
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    WTF_CSRF_ENABLED = True
+    SECRET_KEY = 'AdonbilivitOlusegun'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+# WTF_CSRF_ENABLED = True
+# SECRET_KEY = 'AdonbilivitOlusegun'
