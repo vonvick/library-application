b4b68a35f648b870797a7679bdd856e3e2783487
vonvick <victor.nwaiwu9@gmail.com>
2016-09-22 21:48:52 +0100

    use environment variable to load config files

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,14 @@
 
 
 from flask import Flask
-# import os
+import os
 
 app = Flask(__name__, instance_relative_config=True)
 
-# app.config.from_envvar('APP_SETTINGS')
+app.config.from_object(os.environ.get('APP_SETTINGS'))
 # print(app.config.from_envvar('APP_SETTINGS'))
-# print(os.environ['APP_SETTINGS'])
-app.config.from_object('config.Config')
-app.config.from_pyfile('config.py', silent=True)
+print(os.environ.get('APP_SETTINGS'))
+# app.config.from_object('config.Config')
+# app.config.from_pyfile('config.py', silent=True)
 
 from app import controllers
--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 #!flask/bin/python
 
 from app import app
+import os
+
 # app.debug = True
-# app.run()
+if os.environ.get('APP_SETTINGS') == 'config.DevelopmentConfig':
+    app.run()
