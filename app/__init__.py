5e1c544c7669bfc84dfc1d023a54f60d6e421c0c
vonvick <victor.nwaiwu9@gmail.com>
2016-09-22 12:04:11 +0100

    edit the init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 # app/__init__.py
 
+
 from flask import Flask
+import os
 
 app = Flask(__name__)
+# app.config.from_object(os.environ['APP_SETTINGS'])
+# print(os.environ['APP_SETTINGS'])
 app.config.from_object('config')
 
 from app import controllers
